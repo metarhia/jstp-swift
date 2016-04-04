4ba7f56ee502d61643e3d588bc8d74de40223213
GYFK <notthewhite@gmail.com>
2016-04-04 17:19:15 +0300

    Initial commit
    Working with sole metadata. No caching included

---
+++ JSTPhone/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  JSTPhone
+//
+//  Created by Artem Chernenkiy on 28.03.16.
+//  Copyright © 2016 Test. All rights reserved.
+//
+
+import UIKit
+import JavaScriptCore
+
+class ViewController: UIViewController {
+
+    @IBOutlet var testButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func testRunner(sender: AnyObject) {
+
+        let dataURL     = NSBundle.mainBundle().URLForResource("data", withExtension: "js")!
+        let metadataURL = NSBundle.mainBundle().URLForResource("metadata", withExtension: "js")!
+        let dData       = NSData(contentsOfURL: dataURL)!
+        let dMetadata   = NSData(contentsOfURL: metadataURL)!
+        let data        = NSString(data: dData, encoding: NSUTF8StringEncoding) as! String
+        let metadata    = NSString(data: dMetadata, encoding: NSUTF8StringEncoding) as! String
+
+        var JSDObject = JSTPObject()
+
+        NSLog("logged")
+        for _ in 1...100 {
+            JSDObject = JSTPObject(data: data, metadata: metadata)
+        }
+        NSLog("logged")
+
+        print(JSDObject.JSObject)
+        print(JSDObject["name"]!)
+    }
+}
+
