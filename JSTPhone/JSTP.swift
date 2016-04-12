db43b298e13d21eb3ecfb65885693e607f913557
Nikita Kirichek <nikita.kirichek@gmail.com>
2016-04-12 10:45:40 +0300

    Improved memory management and readability
    Deleted unnecessary properties, improved partition of contexts,
    improved usage of JSTPObject

--- JSTPhone/JSTP.swift
+++ JSTPhone/JSTP.swift
@@ -8,101 +8,58 @@
 
 import JavaScriptCore
 
-private let api = try? String(
+private let stringApi     = try? String(
     contentsOfFile: NSBundle.mainBundle().pathForResource("api", ofType: "js")!,
     encoding: NSUTF8StringEncoding)
 
-private let metaContext   = JSContext().evaluateScript(api).context
-private let context       = JSContext(virtualMachine: metaContext.virtualMachine)
-private let JSQueue       = dispatch_queue_create("JS", DISPATCH_QUEUE_SERIAL)
+private let api = JSContext().evaluateScript(stringApi).context
+private let context = JSContext(virtualMachine: api.virtualMachine)
+
+private let JSQueue = dispatch_queue_create("JS", DISPATCH_QUEUE_SERIAL)
+
 private var metadataCache = [Int : String]()
 
-public class jstp {
 
-    private init() { }
-
-    public static func parse(str: String) -> [AnyObject] {
-        return JSTPOS(data: str).parsed as [AnyObject]
+public struct JSTPObject {
+    
+    public var JSObject: AnyObject!
+    private var id = (NSUUID().UUIDString as NSString).substringToIndex(3)
+    
+    public init() { JSObject = nil }
+    
+    public init(data: NSData, metadata: NSData) {
+        self.init(data: NSString(data: data, encoding: NSUTF8StringEncoding) as! String,
+            metadata: NSString(data: metadata, encoding: NSUTF8StringEncoding) as! String)
     }
-
-    public static func intertprete(str: String) -> NSObject! {
-        return JSTPOS(data: str).interpreted as! NSObject
+    
+    public init(data: String, metadata: String) {
+        let signal = dispatch_semaphore_create(0)
+        
+        dispatch_async(JSQueue) {
+            self.initData(data)
+            self.initMetadata(metadata)
+            self.JSObject = api["jsrd"].callWithArguments(
+                [context["data"], context["meta" + self.id]]).toObject()
+            dispatch_semaphore_signal(signal)
+        };  dispatch_semaphore_wait(signal, DISPATCH_TIME_FOREVER)
     }
-
-    public static func jsrd(data: String, metadata: String) -> NSObject! {
-        return JSTPRM(data: data, metadata: metadata).jsrd as! NSObject
-    }
-}
-
-extension jstp {
-    private static func writeData(data: String) {
-        context.evaluateScript("var data=\(data);")
-    }
-}
-
-
-private struct JSTPOS {
-
-    weak var parsed: NSArray! {
-        let arr = context.objectForKeyedSubscript("data").toArray()
-                  context.deleteJSProperty("data")
-        return arr
-    }
-
-    weak var interpreted: AnyObject! {
-        let obj = context.objectForKeyedSubscript("data").toObject()
-                  context.deleteJSProperty("data")
-        return obj
-    }
-
-    init(data: String) {
-        jstp.writeData(data)
-    }
-}
-
-
-private struct JSTPRM {
-
-    weak var jsrd: AnyObject! {
-        let obj = metaContext.objectForKeyedSubscript("jsrd").callWithArguments(
-                     [context.objectForKeyedSubscript("data"),
-                  metaContext.objectForKeyedSubscript("meta" + self.id)]).toObject()
-                      context.deleteJSProperty("data")
-        return obj
-    }
-    var id = (NSUUID().UUIDString as NSString).substringToIndex(3)
-
-    init(data: NSData, metadata: NSData) {
-        self.init(data: NSString(data:     data, encoding: NSUTF8StringEncoding) as! String,
-              metadata: NSString(data: metadata, encoding: NSUTF8StringEncoding) as! String)
-    }
-
-    init(data: String, metadata: String) {
-        self.writeMetadata(metadata)
-        jstp.writeData(data)
-    }
-
-    /**
-     * TODO: redo metadata initialization.
-     */
-    mutating func writeMetadata(metadata: String) {
+    
+    public func initData(data: String) { context.evaluateScript("var data = \(data)") }
+    
+    private mutating func initMetadata(metadata: String) {
         if let value = metadataCache[metadata.hash] { self.id = value }
         else {
-            metaContext.evaluateScript("var meta" + self.id + "=\(metadata);")
+            context.evaluateScript("var meta" + self.id + " = \(metadata);")
             metadataCache.updateValue(self.id, forKey: metadata.hash)
         }
     }
-}
-
-extension NSObject {
-    public subscript(key: String) -> AnyObject? { return self.valueForKey(key) }
+    
+    public subscript(key: String) -> AnyObject? {
+        if let value =  JSObject.valueForKey(key) { return value }
+        else { return nil }
+    }
 }
 
 extension JSContext {
-    private func deleteJSProperty(name: String) {
-        JSObjectDeleteProperty(self.JSGlobalContextRef,
-                               self.globalObject.JSValueRef,
-                               self.objectForKeyedSubscript(name).JSValueRef, nil)
-              JSGarbageCollect(self.JSGlobalContextRef)
-    }
+    private subscript(key: String) -> JSValue { return self.objectForKeyedSubscript(key) }
 }
--- JSTPhone/ViewController.swift
+++ JSTPhone/ViewController.swift
@@ -10,70 +10,48 @@ import UIKit
 import JavaScriptCore
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet var testButton: UIButton!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     @IBAction func testRunner(sender: AnyObject) {
-
+        
         let dataURL     = NSBundle.mainBundle().URLForResource("data", withExtension: "js")!
         let metadataURL = NSBundle.mainBundle().URLForResource("metadata", withExtension: "js")!
         let dData       = NSData(contentsOfURL: dataURL)!
         let dMetadata   = NSData(contentsOfURL: metadataURL)!
         let data        = NSString(data: dData, encoding: NSUTF8StringEncoding) as! String
         let metadata    = NSString(data: dMetadata, encoding: NSUTF8StringEncoding) as! String
-
-//        var JSDObject = JSTPObject(data: data, metadata: metadata)
-//        let begin = NSDate()
-//        for _ in 1...100000 {
-//            JSDObject = JSTPObject(data: data, metadata: metadata)
-//        }
-//        NSLog("Init time: \((-begin.timeIntervalSinceNow).description)")
-//
-//        print(JSDObject.jsrd)
-
-        let newBegin = NSDate()
-        /* Here comes the lock */
+        
+        var JSDObject = JSTPObject()
+        
+        let startInit = NSDate()
+        
         for _ in 1...100000 {
-            let _ = jstp.jsrd(data, metadata: metadata)
+            JSDObject = JSTPObject(data: data, metadata: metadata)
         }
-        NSLog("Parse time: \((-newBegin.timeIntervalSinceNow).description)")
-        print(jstp.jsrd(data, metadata: metadata))
-        print(jstp.jsrd(data, metadata: metadata)["name"]!)
-
-
-//        let anoBegin = NSDate()
-//        /* Here comes the lock */
-//        for _ in 1...100000 {
-//            let _ = jstp.jsrd(data, metadata: metadata)
-//        }
-//        NSLog("Parse time: \((-anoBegin.timeIntervalSinceNow).description)")
-//        print(jstp.jsrd(data, metadata: metadata))
-//
-
-//        let _anoBegin = NSDate()
-//        /* Here comes the lock */
-//        for _ in 1...100000 {
-//            let _ = jstp.jsrd(data, metadata: metadata)
-//        }
-//        NSLog("Parse time: \((-_anoBegin.timeIntervalSinceNow).description)")
-//        print(jstp.jsrd(data, metadata: metadata))
-//
-
-        let _Begin = NSDate()
+        
+        print("Time of initializing: \(NSDate().timeIntervalSinceDate(startInit))")
+        
+        let startGetting = NSDate()
+        
         for _ in 1...100000 {
-            let _ = jstp.parse(data)
+            let _ = JSDObject.JSObject
         }
-        NSLog("Parse time: \((-_Begin.timeIntervalSinceNow).description)")
-        print(jstp.parse(data))
+        
+        print("Time of getting the object: \(NSDate().timeIntervalSinceDate(startGetting))")
+        
+        print(JSDObject.JSObject)
+        print(JSDObject["name"]!)
     }
 }
+
