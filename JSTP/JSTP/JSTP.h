40b5bf4c8e808ad2d062c6e047f2082e672cb4fb
Andrew Visotskiy <firemaaaan@gmail.com>
2016-07-19 22:34:25 +0300

    initial commit

---
+++ JSTP/JSTP/JSTP.h
@@ -0,0 +1,19 @@
+//
+//  JSTP.h
+//  JSTP
+//
+//  Created by Andrew Visotskyy on 7/19/16.
+//  Copyright © 2016 Andrew Visotskyy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JSTP.
+FOUNDATION_EXPORT double JSTPVersionNumber;
+
+//! Project version string for JSTP.
+FOUNDATION_EXPORT const unsigned char JSTPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JSTP/PublicHeader.h>
+
+
