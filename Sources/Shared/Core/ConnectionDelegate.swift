1a253e2a6b97f5106c84ac124fdad0733f8651bd
Andrew Visotskiy <firemaaaan@gmail.com>
2017-06-13 12:48:34 +0300

    doc: documented connection delegate

--- Sources/Shared/Core/ConnectionDelegate.swift
+++ Sources/Shared/Core/ConnectionDelegate.swift
@@ -8,12 +8,31 @@
 
 public protocol ConnectionDelegate: class {
 
+	/// Called when a connection receives remote event.
+	///
+	/// - Parameters:
+	///   - connection: Connection instance which received event.
+	///   - event: Object holding information about the event.
 	func connection(_ connection: Connection, didReceiveEvent event: Event)
 
+	/// Called in case of non-critical errors occur in connection.
+	///
+	/// - Parameters:
+	///   - connection: Connection instance where error occurred.
+	///   - error: An object representing an error.
 	func connection(_ connection: Connection, didFailWithError error: Error)
 
+	/// Called when a connection disconnects with or without error.
+	///
+	/// - Parameters:
+	///   - connection: Connection instance.
+	///   - error: An optional object representing an error.
 	func connection(_ connection: Connection, didDisconnectWithError error: Error?)
 
+	/// Called when a connection connects and is ready to work.
+	///
+	/// - Parameters:
+	///   - connection: Connection instance.
 	func connectionDidConnect(_ connection: Connection)
 
 }
