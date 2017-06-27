19923ebff832d359090b08aca99967cb3941f1c5
Andrew Visotskiy <firemaaaan@gmail.com>
2017-06-27 16:13:38 +0300

    connection: added buffering policy protocol

---
+++ Sources/Shared/Core/BufferingPolicy.swift
@@ -0,0 +1,15 @@
+//
+//  PacketsBufferingPolicy.swift
+//  JSTP
+//
+//  Created by Andrew Visotskyy on 7/9/17.
+//  Copyright © 2017 Andrew Visotskyy. All rights reserved.
+//
+
+internal protocol BufferingPolicy {
+
+	var buffer: [Packet] { get }
+
+	func buffer(packet: Packet)
+
+}
