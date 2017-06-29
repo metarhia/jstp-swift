c0328866062c760fa358061b4af29f6d536b8e2f
Andrew Visotskiy <firemaaaan@gmail.com>
2017-06-29 17:33:17 +0300

    connection: documented BufferBufferingPolicy

--- Sources/Shared/Core/BufferBufferingPolicy.swift
+++ Sources/Shared/Core/BufferBufferingPolicy.swift
@@ -10,6 +10,13 @@ internal class BufferBufferingPolicy: BufferingPolicy {
 
 	private(set) internal var buffer: [Packet]
 
+	/// The maximum number of objects the buffer should hold.
+	///
+	/// If 0 or less, there is no count limit. The default value is 0.
+	///
+	/// This is not a strict limit — if the buffer goes over the limit, an object in the buffer could
+	/// be evicted instantly, later, or possibly never, depending on the implementation details of
+	/// the buffer.
 	internal var countLimit: Int
 
 	internal func buffer(packet: Packet) {
@@ -23,6 +30,7 @@ internal class BufferBufferingPolicy: BufferingPolicy {
 		}
 	}
 
+	/// Refines buffer due to the maximum number of objects the buffer should hold.
 	private func refineBuffer() {
 		guard countLimit > 0 else {
 			return
@@ -32,6 +40,10 @@ internal class BufferBufferingPolicy: BufferingPolicy {
 
 	// MARK: - Lifecycle
 
+	/// Returns a `BufferingPolicy` object initialized by using a given `countLimit` or default value
+	/// which is equal to 0.
+	///
+	/// - Parameter countLimit: The maximum number of objects the buffer should hold.
 	internal init(with countLimit: Int = 0) {
 		self.buffer = []
 		self.countLimit = countLimit
