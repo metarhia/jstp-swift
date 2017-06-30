6254596008301d57741ea2a5b82a17582f1b3ab5
Andrew Visotskiy <firemaaaan@gmail.com>
2017-06-30 10:03:27 +0300

    connection: added RestorationPolicy class

---
+++ Sources/Shared/Core/RestorationPolicy.swift
@@ -0,0 +1,21 @@
+//
+//  RestorationPolicy.swift
+//  JSTP
+//
+//  Created by Andrew Visotskyy on 6/22/17.
+//  Copyright © 2017 Andrew Visotskyy. All rights reserved.
+//
+
+import Foundation
+
+public class RestorationPolicy {
+
+	internal func onTransportAvailable(connection: Connection, success succeed: @escaping () -> Void, failure fail: @escaping (Error) -> Void) {
+
+	}
+
+	internal func onRestore(connection: Connection, completion: @escaping () -> Void) {
+
+	}
+
+}
