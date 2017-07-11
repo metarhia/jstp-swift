ff44cab0a002bae833e6e202f2c0c9b90d188326
Andrew Visotskiy <firemaaaan@gmail.com>
2017-07-11 20:37:25 +0300

    connection: added string extension to decode UTF-8 data
    Valid for data which may not contain all continuation bytes for last
    character.

---
+++ Sources/Shared/Extensions/String+Decode.swift
@@ -0,0 +1,40 @@
+//
+//  String+Extension.swift
+//  JSTP
+//
+//  Created by Andrew Visotskyy on 8/8/17.
+//  Copyright © 2017 Andrew Visotskyy. All rights reserved.
+//
+
+extension String {
+
+	public static func decode(data: inout Data) -> String? {
+		var headerPosition = data.count - 1
+		while data[headerPosition] & UInt8(0xC0) == UInt8(0x80) {
+			headerPosition -= 1
+		}
+		let header = data[headerPosition]
+		guard let length = String.characterBytesCount(for: header) else {
+			return nil
+		}
+		let validCount = headerPosition + length == data.count ? data.count : headerPosition
+		let validBytes = data.prefix(upTo: validCount)
+		data.removeFirst(validCount)
+		return String(bytes: validBytes, encoding: .utf8)
+	}
+
+	private static func characterBytesCount(for header: UInt8) -> Int? {
+		if header & UInt8(0x80) == UInt8(0) {
+			return 1
+		} else if header & UInt8(0xE0) == UInt8(0xC0) {
+			return 2
+		} else if header & UInt8(0xF0) == UInt8(0xE0) {
+			return 3
+		} else if header & UInt8(0xF8) == UInt8(0xF0) {
+			return 4
+		} else {
+			return nil
+		}
+	}
+
+}
